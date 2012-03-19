c9b665e9000d5a610f69de5af55d8e6e30e90800
Bad-Robot <mat.roscoe@unb.ca>
2012-03-18 21:18:24 -0300

    Added game_board initilization

--- game_board.v
+++ game_board.v
@@ -15,6 +15,17 @@ module game_board
 //       4   3210 
 reg [4:0] game_board [0:18] [0:18];
 
+initial begin
+	integer x; 
+	integer y; 
+	
+	for( x = 0; x < 19; x = x + 1 ) begin
+		for( y = 0; y < 19; y = y + 1 ) begin
+			game_board[x][y] = 6; 
+		end
+	end
+end
+
 task modify_board; 
 	input [0:5] x; 
 	input [0:5] y; 
@@ -40,50 +51,62 @@ task play_move;
 	
 	begin
 		while( moves_left > 0 && moves_left <= 2 ) begin
-		// look for WIN condition
-		
-		for( x = 0; x < 19; x = x + 1 ) begin
-			for( y = 0; y < 19; y = y + 1 ) begin
+			// look for WIN condition
+			
+			for( x = 0; x < 19; x = x + 1 ) begin
+				for( y = 0; y < 19; y = y + 1 ) begin
+					
+				end
+			end
+			
+			if( moves_left > 0 ) begin
+				// look for defensive plays
+				/*
+					Need to add a control structure for restarting 
+					the search in order to make sure we always get
+					the proper move to play. 
+				*/
+				for( x = 0; x < 19; x = x + 1 ) begin
+					for( y = 0; y < 19; y = y + 1 ) begin
+						current_weight = game_board[x][y]; 
+						if( current_weight == 11 ) begin
+							modify_board( x, y, 1 ); 
+							moves_left = moves_left -1; 
+						end
+						else if( current_weight == 10 ) begin
+							modify_board( x, y, 1 ); 
+							moves_left = moves_left -1; 
+						end
+						else begin
+							// keep calm and carry on. 
+						end
+					end
+				end
 				
-			end
-		end
-		
-		// look for defensive plays
-		for( x = 0; x < 19; x = x + 1 ) begin
-			for( y = 0; y < 19; y = y + 1 ) begin
-				current_weight = game_board[x][y]; 
-				if( current_weight == 11 ) begin
-					modify_board( x, y, 1 ); 
-					moves_left = moves_left -1; 
-				end
-				else if( current_weight == 10 ) begin
-					modify_board( x, y, 1 ); 
-					moves_left = moves_left -1; 
-				end
-				else begin
-				end
-			end
-		end
-		
-		// look for offsensive moves :) 
-		
-		for( x = 0; x < 19; x = x + 1 ) begin
-			for( y = 0; y < 19; y = y + 1 ) begin
-				current_weight = game_board[x][y]; 
-				if( current_weight == 3 ) begin
-					modify_board( x, y, 1 ); 
-					moves_left = moves_left -1; 
-				end
-				else if( current_weight == 2 ) begin
-					modify_board( x, y, 1 ); 
-					moves_left = moves_left -1; 
-				end
-				else begin
+				if( moves_left > 0 ) begin
+					// look for offsensive moves :) 
+					/*
+						Need to add a control structure like the one 
+						talked about above.  
+					*/
+					for( x = 0; x < 19; x = x + 1 ) begin
+						for( y = 0; y < 19; y = y + 1 ) begin
+							current_weight = game_board[x][y]; 
+							if( current_weight == 3 ) begin
+								modify_board( x, y, 1 ); 
+								moves_left = moves_left -1; 
+							end
+							else if( current_weight == 2 ) begin
+								modify_board( x, y, 1 ); 
+								moves_left = moves_left -1; 
+							end
+							else begin
+								// keep calm and carray on.
+							end
+						end
+					end
 				end
 			end
-		end
-		
-		//modify_board( x, y, 1 ); 
 		end
 	end
 endtask
